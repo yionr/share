d5072d0320df5bb2ebbcdb728f843ee1eb76ff95
yionr <yionr99@gmail.com>
2021-02-26 14:37:25 +0800

    注册功能已经基本完善了，实现了在激活之前会阻止登录，激活成功会有提示，并跳转登录等功能

--- src/main/java/cn/yionr/share/service/impl/UserServiceImpl.java
+++ src/main/java/cn/yionr/share/service/impl/UserServiceImpl.java
@@ -42,20 +42,23 @@ public class UserServiceImpl implements UserService {
             } else {
                 if (Duration.between(LocalDateTime.ofInstant(Instant.ofEpochMilli(userInDB.getCreated_time()), ZoneId.systemDefault()), LocalDateTime.now()).toDays() >= 2) {
 //                    刷新数据库中该用户的创建时间，重新给用户发送激活信息，要求用户重新注册。
-
                     sendMail(user);
+                    return userMapper.updateActiveTime(user);
                 } else {
 //                    提醒用户去邮箱内激活
                     throw new UserNotActiveException("用户任在激活期间内，但还未激活");
                 }
             }
         }
+        else{
 //        用户注册邮箱，有效性验证要在两天内完成，为此，数据库要添加几个字段 注册时间； 是否已经注册：已注册-待激活 两个状态；
 //        如果有新的注册请求，去查询用户的时候如果查到了但是发现是待激活状态的，判断日期是否超过两天，
 //        如果两天内则提醒用户去邮箱查看；
 //        如果超过两天则删除记录，并重新开始注册流程（递归）
-        sendMail(user);
-        return userMapper.addUser(user);
+            sendMail(user);
+            return userMapper.addUser(user);
+
+        }
     }
 
     @Override
@@ -72,7 +75,7 @@ public class UserServiceImpl implements UserService {
      * @param uuid 激活码为创建日期进行加密的结果
      */
     @Override
-    public void active(String email, String uuid) throws ActiveLinkOutOfDateException, UserWaitToActiveNotFoundException, UUIDInvalidException, UserActivedException {
+    public User active(String email, String uuid) throws ActiveLinkOutOfDateException, UserWaitToActiveNotFoundException, UUIDInvalidException, UserActivedException {
 
         User user = userMapper.queryUser(email);
         if (user != null) {
@@ -82,16 +85,16 @@ public class UserServiceImpl implements UserService {
             } else {
 //                根据time计算uuid是否和传来的uuid一致，不一致的话报错
                 if (!user.isActive()) {
-                    log.info("激活码为： " + DigestUtils.md5DigestAsHex((user.getCreated_time() + "").getBytes(StandardCharsets.UTF_8)));
                     if (DigestUtils.md5DigestAsHex((user.getCreated_time() + "").getBytes(StandardCharsets.UTF_8)).equals(uuid)) {
+                        log.info("激活成功");
                         userMapper.active(email);
+                        return user;
                     } else {
                         throw new UUIDInvalidException("该激活码无效");
                     }
                 } else {
                     throw new UserActivedException("用户已注册");
                 }
-
             }
         } else {
             throw new UserWaitToActiveNotFoundException("没有此待激活用户");
