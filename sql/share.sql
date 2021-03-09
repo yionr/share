a9926b04382a3c325f9693b70c8c32b29a0a813c
yionr <yionr99@gmail.com>
2021-03-09 16:52:28 +0800

    更新sql文件

--- sql/share.sql
+++ sql/share.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : ecs
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 50733
- Source Host           : yionr.cn:3306
+ Source Host           : localhost:3306
  Source Schema         : share
 
  Target Server Type    : MySQL
  Target Server Version : 50733
  File Encoding         : 65001
 
- Date: 02/03/2021 19:14:06
+ Date: 09/03/2021 16:51:41
 */
 
 SET NAMES utf8mb4;
@@ -22,14 +22,15 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sfile`;
 CREATE TABLE `sfile`  (
-                          `fid` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                          `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                          `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-                          `times` int(11) NOT NULL DEFAULT 1,
-                          `filetype` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT 'file',
-                          `uid` int(11) NULL DEFAULT NULL,
-                          PRIMARY KEY (`fid`) USING BTREE,
-                          UNIQUE INDEX `sFile_fid_uindex`(`fid`) USING BTREE
+  `fid` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `times` int(11) NOT NULL DEFAULT 1,
+  `uid` int(11) NULL DEFAULT -1,
+  `filetype` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT 'file',
+  `uploaded_time` bigint(14) NOT NULL,
+  PRIMARY KEY (`fid`) USING BTREE,
+  UNIQUE INDEX `sFile_fid_uindex`(`fid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -37,14 +38,14 @@ CREATE TABLE `sfile`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-                         `uid` int(11) NOT NULL AUTO_INCREMENT,
-                         `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                         `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                         `created_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                         `active` tinyint(1) NULL DEFAULT 0,
-                         PRIMARY KEY (`uid`) USING BTREE,
-                         UNIQUE INDEX `user_email_uindex`(`email`) USING BTREE,
-                         UNIQUE INDEX `user_uid_uindex`(`uid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_time` bigint(14) NOT NULL DEFAULT 0,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`uid`) USING BTREE,
+  UNIQUE INDEX `user_email_uindex`(`email`) USING BTREE,
+  UNIQUE INDEX `user_uid_uindex`(`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
