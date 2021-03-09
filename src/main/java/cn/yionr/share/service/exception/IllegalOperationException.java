04e05ce0d7242f98ac4673064f54946a2a64ab87
yionr <yionr99@gmail.com>
2021-03-09 15:55:55 +0800

    IllegalOperationException 当发生非法操作的时候抛出

---
+++ src/main/java/cn/yionr/share/service/exception/IllegalOperationException.java
@@ -0,0 +1,7 @@
+package cn.yionr.share.service.exception;
+
+public class IllegalOperationException extends Exception {
+    public IllegalOperationException(String s) {
+        super(s);
+    }
+}
